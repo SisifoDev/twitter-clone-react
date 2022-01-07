6def7577e3640160c460cbe01995cfd49bc854c0
Manuel Perdomo <manuelperdomo1710@icloud.com>
2022-01-07 12:50:41 -0500

    [Add] Firebase

---
+++ firebase/client.js
@@ -0,0 +1,10 @@
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQt2NdNO0udQQhuYxt_x7tBPef1RNbblE",
+  authDomain: "twitter-clone-5292f.firebaseapp.com",
+  projectId: "twitter-clone-5292f",
+  storageBucket: "twitter-clone-5292f.appspot.com",
+  messagingSenderId: "75902734715",
+  appId: "1:75902734715:web:8ecdba2c8bc68b93b1adde",
+};
